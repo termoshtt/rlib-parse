acf8b0b6cd2f6018bb6b2658c66004c79b9aeada
Toshiki Teramura <toshiki.teramura@gmail.com>
2017-12-08 10:38:19 +0900

    Use subcommand

--- src/main.rs
+++ src/main.rs
@@ -4,13 +4,15 @@ extern crate flate2;
 
 pub mod bytecode;
 
-use clap::{App, Arg};
+use clap::*;
 
 use std::fs::File;
 use std::io::Read;
 use std::path::*;
 
-fn read_rlib(filename: &Path) -> Result<Vec<u8>, Box<::std::error::Error>> {
+type Result<T> = ::std::result::Result<T, Box<::std::error::Error>>;
+
+fn read_rlib(filename: &Path) -> Result<Vec<u8>> {
     let mut f = File::open(filename)?;
     let mut buf = Vec::new();
     f.read_to_end(&mut buf)?;
@@ -18,11 +20,15 @@ fn read_rlib(filename: &Path) -> Result<Vec<u8>, Box<::std::error::Error>> {
 }
 
 fn main() {
-    let app =
-        App::new("cargo-nvbuild").arg(Arg::with_name("input").help("Input *.rlib file").required(
-            true,
-        ));
-    let matches = app.get_matches();
+    let matches = App::new("cargo-nvbuild")
+        .bin_name("cargo")
+        .subcommand(SubCommand::with_name("nvbuild").arg(
+            Arg::with_name("input").help("Input *.rlib file").required(
+                true,
+            ),
+        ))
+        .get_matches();
+    let matches = matches.subcommand_matches("nvbuild").unwrap();
 
     let input = matches.value_of("input").unwrap();
     let bin = read_rlib(Path::new(input)).unwrap();
