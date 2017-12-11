5454c7c21f93791464bfbb7ca8a67f8fd86ff438
Toshiki Teramura <toshiki.teramura@gmail.com>
2017-12-11 14:30:56 +0900

    Output *.bc

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ pub mod bytecode;
 use clap::*;
 
 use std::fs::File;
-use std::io::Read;
+use std::io::{Read, Write};
 use std::path::*;
 
 type Result<T> = ::std::result::Result<T, Box<::std::error::Error>>;
@@ -19,20 +19,22 @@ fn read_rlib(filename: &Path) -> Result<Vec<u8>> {
     Ok(buf)
 }
 
+fn write_bc(filename: &Path, data: &[u8]) -> Result<()> {
+    let mut f = File::create(filename)?;
+    f.write_all(data)?;
+    Ok(())
+}
+
 fn main() {
-    let matches = App::new("cargo-nvbuild")
-        .bin_name("cargo")
-        .subcommand(SubCommand::with_name("nvbuild").arg(
-            Arg::with_name("input").help("Input *.rlib file").required(
-                true,
-            ),
+    let matches = App::new("nvbuild")
+        .arg(Arg::with_name("input").help("Input *.rlib file").required(
+            true,
         ))
         .get_matches();
-    let matches = matches.subcommand_matches("nvbuild").unwrap();
-
     let input = matches.value_of("input").unwrap();
     let bin = read_rlib(Path::new(input)).unwrap();
-    let bc = bytecode::DecodedBytecode::new(&bin).unwrap();
-    println!("Identifier = {:?}", bc.identifier());
-    println!("ByteCode   = {:?}", bc.bytecode());
+    let decoded = bytecode::DecodedBytecode::new(&bin).unwrap();
+    let name = format!("{}.bc", decoded.identifier());
+    let bc = decoded.bytecode();
+    write_bc(&Path::new(&name), &bc).unwrap();
 }
